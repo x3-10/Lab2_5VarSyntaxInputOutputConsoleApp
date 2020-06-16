707515221bace23d7e3d2970887bd9204ad9f9a5
310lbenson@gmail.com <310lbenson@gmail.com>
2020-06-16 13:13:23 -0400

    Lab2_5 allows the user to see how input and output works in C#

--- Lab2_5VarSyntaxInputOutputConsoleApp/Program.cs
+++ Lab2_5VarSyntaxInputOutputConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Lab2_5VarSyntaxInputOutputConsoleApp
 {
@@ -9,13 +10,40 @@ namespace Lab2_5VarSyntaxInputOutputConsoleApp
             Console.WriteLine("Hello World!");
             // Lab 2_5
 
-            // 1. Write a C# program to print on screen the output of adding, subtracting, multiplying and dividing of two numbers which will be entered by the user.
+            // 1. Write a C# program to print on screen the output of adding, subtracting, 
+            //  multiplying and dividing of two numbers which will be entered by the user.
+            int intFirstNumber = 0;
+            int intSecondNumber = 0;
+            int intResult = 0;
+            double dblResult = 0;
+
+            Console.WriteLine("Enter your first number: ");
+            intFirstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number: ");
+            intSecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Adding
+            intResult = intFirstNumber + intSecondNumber;
+            Console.WriteLine($"{intFirstNumber} plus {intSecondNumber} is {intResult}");
+
+            //Subtracting
+            intResult = intFirstNumber - intSecondNumber;
+            Console.WriteLine($"{intFirstNumber} minus {intSecondNumber} is {intResult}");
+
+            //Multiplying
+            intResult = intFirstNumber * intSecondNumber;
+            Console.WriteLine($"{intFirstNumber} multiplied by {intSecondNumber} is {intResult}");
+
+            //Dividing
+            intResult = intFirstNumber / intSecondNumber;
+            Console.WriteLine($"{intFirstNumber} divided by {intSecondNumber} is {intResult}");
 
             // 2. Write a C# program to print Hello and your name (which you will type in on the Console) in a separate line.
 
             // 3. Write a C# program to print the output of multiplication of three numbers which will be entered by the user.
 
-            // 4. Write a C# program that takes an age (for example 20) as input and prints something like "You lo
+
             // 4. Write a C# program that takes an age (for example 20) as input and prints something like "You look younger than 20 (the age the user entered)".
         }
     }
